f6329ff090f8783be21122c0c8352311124f6be6
Philipp Staudinger <philipp.eckhard.staudinger@gmail.com>
2022-11-09 20:35:27 +0100

    jetzt ist es so clean ihr könnt mir danken

--- CoC.py
+++ CoC.py
@@ -2,17 +2,20 @@ import Attack as a
 import Upgrade as u
 import keyboard
 import Finder as f
+import ImageManager as im
 
 
 print("start")
 while(keyboard.is_pressed('q') == False):
     pass
 
-#while(not a.army_done()):
-    #pass
-#a.attack()
+while(True):
+    while(not a.army_done()):
+        pass
+    a.attack()
 
-a.check_army()
+
+
 
 
 
