b7f078cd12275a35bb97cca36ebfbf19f26aef29
alex-909 <alex.strauss05@gmx.net>
2022-11-16 17:17:07 +0100

    tesseract tests

--- TesseractTest.py
+++ TesseractTest.py
@@ -17,12 +17,17 @@ def get_mousepos():
             return (mouse1, mouse2)
 
 
-
+print("start")
 while(True):
     (pos1, pos2) = get_mousepos()
     im = ImageGrab.grab(bbox=(pos1[0], pos1[1], pos2[0], pos2[1]))
     im.save(root + r"\temp.png")
-    im.show()
-
     imread =  cv2.imread(root + r"\temp.png")
-    print(f.read_text())
+    imread = f.filter_pixels(imread, 255, 255, 255)
+
+    s = f.read_text(imread)
+    #s = f.text_to_Int(s)
+    print(s)
+    print("a")
+
+    
