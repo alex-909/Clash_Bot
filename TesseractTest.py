0d79872e350a7f4d8a366cd01c953b51c7978411
alex-909 <alex.strauss05@gmx.net>
2022-11-16 17:05:01 +0100

    Create Tests

---
+++ TesseractTest.py
@@ -0,0 +1,28 @@
+import cv2
+import pyscreenshot as ImageGrab
+import Finder as f
+import pyautogui
+import keyboard
+import time
+
+tesseract_path = r"C:\Users\Alex\AppData\Local\Tesseract-OCR\tesseract.exe"
+root = r"images"
+
+def get_mousepos():
+    while(True):
+        if keyboard.is_pressed('q'):
+            mouse1 = pyautogui.position()
+            time.sleep(1)
+            mouse2 = pyautogui.position()
+            return (mouse1, mouse2)
+
+
+
+while(True):
+    (pos1, pos2) = get_mousepos()
+    im = ImageGrab.grab(bbox=(pos1[0], pos1[1], pos2[0], pos2[1]))
+    im.save(root + r"\temp.png")
+    im.show()
+
+    imread =  cv2.imread(root + r"\temp.png")
+    print(f.read_text())
