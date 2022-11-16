317a4341ccc3c52c1aeccb574ba8bd9032cf7033
alex-909 <alex.strauss05@gmx.net>
2022-11-16 17:17:54 +0100

    stashed

--- TesseractTest.py
+++ TesseractTest.py
@@ -17,11 +17,16 @@ def get_mousepos():
             return (mouse1, mouse2)
 
 
+<<<<<<< Updated upstream
 print("start")
+=======
+
+>>>>>>> Stashed changes
 while(True):
     (pos1, pos2) = get_mousepos()
     im = ImageGrab.grab(bbox=(pos1[0], pos1[1], pos2[0], pos2[1]))
     im.save(root + r"\temp.png")
+<<<<<<< Updated upstream
     imread =  cv2.imread(root + r"\temp.png")
     imread = f.filter_pixels(imread, 255, 255, 255)
 
@@ -31,3 +36,9 @@ while(True):
     print("a")
 
     
+=======
+    im.show()
+
+    imread =  cv2.imread(root + r"\temp.png")
+    print(f.read_text())
+>>>>>>> Stashed changes
